5cbbcef9eb8fc9661fe8c60224a6a67aa26e70b1
al8032pa <al8032pa@eit000211.pc.lu.se>
2025-02-19 15:24:19 +0100

    jobresults directory for cleanliness

--- DD2358_Proj/Project/jobscript.sh
+++ DD2358_Proj/Project/jobscript.sh
@@ -6,8 +6,8 @@
 #SBATCH -N 1 ##number of nodes that will be allocated - must use --ntasks-per-node or --cpus-per-task to use more than 1 core per node
 #SBATCH --tasks-per-node=48 ##number of cores used per task? - up to 48 per node for COSMOS. Presumably this is what I want, using MPI
 
-#SBATCH -o %j_result.out ## result filename, %j becomes the job number
-#SBATCH -e %j_result.err ## errors filename - should be empty unless an error occurs
+#SBATCH -o /jobresults/%j_result.out ## result filename, %j becomes the job number
+#SBATCH -e /jobresults/%j_result.err ## errors filename - should be empty unless an error occurs
 #SBATCH -J oom_test_morecpn ##puts a job name, to identify it
 
 cat $0 ## unix command - outputs this script to the top of the job's output file
