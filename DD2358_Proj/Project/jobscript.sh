41eff0e579e6d0777a9461e2c1d0f310877b301e
al8032pa <alexandros.pallaris@eit.lth.se>
2025-03-05 13:20:14 +0100

    MPI 1 run

--- DD2358_Proj/Project/jobscript.sh
+++ DD2358_Proj/Project/jobscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ### SBATCH # --qos=test  ## to run a quick, high-priority test (should be very quick)
-#SBATCH -t 18:45:00 ## job is killed after this time - overestimate
+#SBATCH -t 30:45:00 ## job is killed after this time - overestimate
 #SBATCH -A lu2024-2-93 ##from projinfo command
 
 #SBATCH -N 1 ##number of nodes that will be allocated - must use --ntasks-per-node or --cpus-per-task to use more than 1 core per node
@@ -8,7 +8,7 @@
 
 #SBATCH -o jobresults/%j_result.out ## result filename, %j becomes the job number
 #SBATCH -e jobresults/%j_result.err ## errors filename - should be empty unless an error occurs
-#SBATCH -J memsTimesTesting_MPI12 ##puts a job name, to identify it
+#SBATCH -J MPI1 ##puts a job name, to identify it
 
 cat $0 ## unix command - outputs this script to the top of the job's output file
 
@@ -27,7 +27,7 @@ cp -p data3D/prevRuns.prev $SNIC_TMP"/data3D"
 cd $SNIC_TMP ## go to that directory to run the script
 
 #time python Scatt3D.py ### then run it... and time it
-export MPInum=12 ## number of MPI processes
+export MPInum=1 ## number of MPI processes
 time mpirun -n $MPInum python runScatt3D.py ## run the main process, and time it
 
 # cp -p prevRuns.info $SLURM_SUBMIT_DIR ## copies this file out to whatever directory you were in when using sbatch jobscript.sh
