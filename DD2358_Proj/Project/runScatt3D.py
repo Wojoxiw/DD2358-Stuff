9a14b3b4c5af54059709c9efbbe3379bd2416bc3
al8032pa <alexandros.pallaris@eit.lth.se>
2025-03-20 13:04:06 +0100

    moreenvs

--- DD2358_Proj/Project/runScatt3D.py
+++ DD2358_Proj/Project/runScatt3D.py
@@ -6,7 +6,9 @@
 # Alexandros Pallaris, after that
 
 import os
-#os.environ["OMP_NUM_THREADS"] = "1" # perhaps needed for MPI speedup if using many processes?
+os.environ["OMP_NUM_THREADS"] = "1" # perhaps needed for MPI speedup if using many processes?
+os.environ['MKL_NUM_THREADS'] = '1' # maybe also relevent
+os.environ['NUMEXPR_NUM_THREADS'] = '1' # maybe also relevent
 from mpi4py import MPI
 import numpy as np
 import dolfinx, ufl, basix
