bd8457239cd81db58f51b94a4169ffc1f067f81a
al8032pa <alexandros.pallaris@eit.lth.se>
2025-04-09 14:02:06 +0200

    etttet3

--- DD2358_Proj/Project/runScatt3D.py
+++ DD2358_Proj/Project/runScatt3D.py
@@ -6,13 +6,13 @@
 # Alexandros Pallaris, after that
 
 import os
+import numpy as np
+import dolfinx, ufl, basix
+import dolfinx.fem.petsc
 #os.environ["OMP_NUM_THREADS"] = "1" # perhaps needed for MPI speedup if using many processes?
 #os.environ['MKL_NUM_THREADS'] = '1' # maybe also relevent
 #os.environ['NUMEXPR_NUM_THREADS'] = '1' # maybe also relevent
 from mpi4py import MPI
-import numpy as np
-import dolfinx, ufl, basix
-import dolfinx.fem.petsc
 import gmsh
 from matplotlib import pyplot as plt
 import pyvista as pv
@@ -109,10 +109,10 @@ if __name__ == '__main__':
         prob.calcFarField(reference=True, angles = angles, compareToMie = True)
     
     
-    #testRun()
+    testRun()
     #profilingMemsTimes()
     #actualProfilerRunning()
-    testFarField()
+    #testFarField()
     
     otherprevs = []
     #prevRuns = memTimeEstimation.runTimesMems(folder, comm, otherPrevs = otherprevs)
