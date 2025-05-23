d5345052ab56b4a55e2a16fdbdf9a35ba7f85f96
al8032pa <alexandros.pallaris@eit.lth.se>
2025-05-23 15:41:44 +0200

    sdfsdf

--- DD2358_Proj/Project/runScatt3D.py
+++ DD2358_Proj/Project/runScatt3D.py
@@ -145,8 +145,8 @@ if __name__ == '__main__':
             elif(convergence == 'pmlR0'):
                 probOptions = dict(PML_R0 = ks[i])
                 
-            refMesh = meshMaker.MeshData(comm, reference = True, viewGMSH = False, h=1/12, verbosity = verbosity, N_antennas=0, object_radius = .33, PML_thickness=0.5, domain_radius=0.9, domain_geom='sphere', FF_surface = True, **meshOptions)
-            prob = scatteringProblem.Scatt3DProblem(comm, refMesh, verbosity = verbosity, name=runName, MPInum = MPInum, makeOptVects=False, excitation = 'planewave', material_epsr=2.0, Nf=1, fem_degree=2, **probOptions)
+            refMesh = meshMaker.MeshData(comm, reference = True, viewGMSH = False, h=1/20, verbosity = verbosity, N_antennas=0, object_radius = .33, PML_thickness=0.5, domain_radius=0.9, domain_geom='sphere', FF_surface = True, **meshOptions)
+            prob = scatteringProblem.Scatt3DProblem(comm, refMesh, verbosity = verbosity, name=runName, MPInum = MPInum, makeOptVects=False, excitation = 'planewave', material_epsr=2.0, Nf=1, fem_degree=1, **probOptions)
             newval, khats, farfields, mies = prob.calcFarField(reference=True, compareToMie = False, showPlots=False, returnConvergenceVals=True) ## each return is FF surface area, khat integral at each angle, farfields+mies at each angle
             if(comm.rank == model_rank): ## only needed for main process
                 areaVals.append(newval)
@@ -197,7 +197,7 @@ if __name__ == '__main__':
             ax1.set_yscale('log')
             ax1.legend()
             fig1.tight_layout()
-            plt.savefig(prob.dataFolder+prob.name+convergence+'degree2hover12convergence.png')
+            plt.savefig(prob.dataFolder+prob.name+convergence+'degree1hover20convergence.png')
             #plt.show()
         
     #testRun(h=1/20)
